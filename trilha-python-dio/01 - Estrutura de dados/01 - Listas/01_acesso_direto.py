44d186b2ec1375fbb15e9b5142b834cfcc5c5512
paulognb <paulonany@gmail.com>
2026-04-15 20:57:26 -0300

    transformando subdiretorios em pastas comuns

---
+++ trilha-python-dio/01 - Estrutura de dados/01 - Listas/01_acesso_direto.py
@@ -0,0 +1,12 @@
+frutas = ["maçã", "laranja", "uva", "pera"]
+
+print(frutas[0])  # maçã
+print(frutas[2])  # uva
+
+string_in_list = list("Python") 
+
+print(string_in_list)
+
+carros = ("gol") 
+
+print(isinstance(carros, tuple))
